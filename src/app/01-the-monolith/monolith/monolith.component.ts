35aacfeb0ccee3c2351f4f4acffecda522c0a979
pjnalls <pjnalls.js@gmail.com>
2022-01-06 11:34:38 -0600

    `ng g c` MonolithComponent; +`.html`, `.ts` code.

---
+++ src/app/01-the-monolith/monolith/monolith.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monolith',
+  templateUrl: './monolith.component.html',
+  styleUrls: ['./monolith.component.scss'],
+})
+export class MonolithComponent implements OnInit {
+  list = [
+    { task: 'Go to the store.' },
+    { task: 'Take out the garbage.' },
+    { task: 'Go to sleep early.' },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
