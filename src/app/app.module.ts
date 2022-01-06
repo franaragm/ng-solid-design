bfb27a064fa492b748b6c4d014ba4f1786da9ed1
pjnalls <pjnalls.js@gmail.com>
2022-01-06 17:49:12 -0600

    Add missing modules for reactive forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { MonolithComponent } from './01-the-monolith/monolith/monolith.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MonolithComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MonolithComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
