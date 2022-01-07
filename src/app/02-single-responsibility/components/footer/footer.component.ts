e1541b94cb2798af92639aabfe8da4a15a603a6c
pjnalls <pjnalls.js@gmail.com>
2022-01-07 05:58:08 -0600

    Add `footer.component.ts` functionality.

---
+++ src/app/02-single-responsibility/components/footer/footer.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit {
+  form = this.formBuilder.group({
+    task: '',
+  });
+  submitButtonText = 'ADD TODO';
+  constructor(
+    private taskService: TaskService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.taskService.list.length !== 99) {
+      this.taskService.list.push(this.form.value);
+    }
+  }
+}
