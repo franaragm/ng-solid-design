8169d6323c2ec447c99ec17ee30daf39036e548c
pjnalls <pjnalls.js@gmail.com>
2022-01-07 05:53:45 -0600

    + components for `02-single-responsibility` phase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MonolithComponent } from './01-the-monolith/monolith/monolith.component';
+import { HeaderComponent } from './02-single-responsibility/components/header/header.component';
+import { MainComponent } from './02-single-responsibility/components/main/main.component';
+import { FooterComponent } from './02-single-responsibility/components/footer/footer.component';
+import { SingleResponsibilityComponent } from './02-single-responsibility/single-responsibility/single-responsibility.component';
 
 @NgModule({
-  declarations: [AppComponent, MonolithComponent],
+  declarations: [
+    AppComponent,
+    MonolithComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    SingleResponsibilityComponent,
+  ],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
