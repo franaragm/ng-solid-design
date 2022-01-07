638f76a1f2a18fb263ef0801240a7bdbac1d0720
pjnalls <pjnalls.js@gmail.com>
2022-01-07 06:05:42 -0600

    + basic functionality for component.

---
+++ src/app/02-single-responsibility/single-responsibility/single-responsibility.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-responsibility',
+  templateUrl: './single-responsibility.component.html',
+  styleUrls: ['./single-responsibility.component.scss'],
+})
+export class SingleResponsibilityComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
