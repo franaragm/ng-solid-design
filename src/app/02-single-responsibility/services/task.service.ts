6386e1afd8ad6554316e24f23ff0578b3925379a
pjnalls <pjnalls.js@gmail.com>
2022-01-07 06:02:23 -0600

    Add `TaskService` for `list` of tasks.

---
+++ src/app/02-single-responsibility/services/task.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  list = [
+    { task: 'Go to the store.' },
+    { task: 'Take out the garbage.' },
+    { task: 'Go to sleep early.' },
+  ];
+  constructor() {}
+}
