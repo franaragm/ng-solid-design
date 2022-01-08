53fc063ac612a7c813b88114294c3c6a5b83ec54
pjnalls <pjnalls.js@gmail.com>
2022-01-08 04:42:40 -0600

    Add `title` input property.

--- src/app/03-open-closed/components/section-content/section-content.component.ts
+++ src/app/03-open-closed/components/section-content/section-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-content',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-content.component.scss'],
 })
 export class SectionContentComponent implements OnInit {
+  @Input() title: string = '';
+  
   constructor() {}
 
   ngOnInit(): void {}
