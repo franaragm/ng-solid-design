5d86d940400be086ef16bc6a101b730819a6f521
pjnalls <pjnalls.js@gmail.com>
2022-01-08 06:20:26 -0600

    Add a base class `@Directive()`.

--- src/app/04-liskov-substitution/classes/section-base.ts
+++ src/app/04-liskov-substitution/classes/section-base.ts
@@ -1,2 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive()
 export class SectionBase {
+  list: Array<{ task: string }> = [{ task: '' }];
+
+  deleteOnClick(index: number): void {
+    this.list.splice(index, 1);
+  }
 }
