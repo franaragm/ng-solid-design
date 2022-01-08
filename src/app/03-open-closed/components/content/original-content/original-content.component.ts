c593c41976407559e4b26d2f40bb896b1adcd68d
pjnalls <pjnalls.js@gmail.com>
2022-01-08 06:21:16 -0600

    Extend `SectionBase` class.

--- src/app/03-open-closed/components/content/original-content/original-content.component.ts
+++ src/app/03-open-closed/components/content/original-content/original-content.component.ts
@@ -1,21 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/02-single-responsibility/services/task.service';
+import { SectionBase } from 'src/app/04-liskov-substitution/classes/section-base';
 
 @Component({
   selector: 'app-original-content',
   templateUrl: './original-content.component.html',
-  styleUrls: ['./original-content.component.scss']
+  styleUrls: ['./original-content.component.scss'],
 })
-export class OriginalContentComponent implements OnInit {
-  list: Array<{ task: string }> = [{ task: '' }];
-
-  constructor(private taskService: TaskService) {}
-
+export class OriginalContentComponent extends SectionBase implements OnInit {
+  constructor(private taskService: TaskService) {
+    super();
+  }
   ngOnInit(): void {
     this.list = this.taskService.list;
-  }
-
-  deleteOnClick(index: number): void {
-    this.list.splice(index, 1);
   }
 }
--- src/app/03-open-closed/components/content/square-content/square-content.component.ts
+++ src/app/03-open-closed/components/content/square-content/square-content.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/02-single-responsibility/services/task.service';
+import { SectionBase } from 'src/app/04-liskov-substitution/classes/section-base';
 
 @Component({
   selector: 'app-square-content',
   templateUrl: './square-content.component.html',
   styleUrls: ['./square-content.component.scss'],
 })
-export class SquareContentComponent implements OnInit {
-  list: Array<{ task: string }> = [{ task: '' }];
-
-  constructor(private taskService: TaskService) {}
-
+export class SquareContentComponent extends SectionBase implements OnInit {
+  constructor(private taskService: TaskService) {
+    super();
+  }
   ngOnInit(): void {
     this.list = this.taskService.list;
   }
