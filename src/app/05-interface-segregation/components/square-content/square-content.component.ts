eff562a14e3b35db2d4f5ded4b44b81c44bc6365
pjnalls <pjnalls.js@gmail.com>
2022-01-09 08:12:20 -0600

    Extend `SectionBase`.

--- src/app/05-interface-segregation/components/square-content/square-content.component.ts
+++ src/app/05-interface-segregation/components/square-content/square-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/02-single-responsibility/services/task.service';
+import { SectionBase } from 'src/app/04-liskov-substitution/classes/section-base';
 import { Tasks } from '../../interfaces/tasks';
 
 @Component({
@@ -7,10 +8,13 @@ import { Tasks } from '../../interfaces/tasks';
   templateUrl: './square-content.component.html',
   styleUrls: ['./square-content.component.scss'],
 })
-export class SquareContentComponent implements OnInit, Tasks {
-  tasks: { task: string }[] = [{ task: '' }];
-
-  constructor(private taskService: TaskService) {};
+export class SquareContentComponent
+  extends SectionBase
+  implements OnInit, Tasks
+{
+  constructor(private taskService: TaskService) {
+    super();
+  }
 
   ngOnInit(): void {
     this.tasks = this.taskService.tasks;
