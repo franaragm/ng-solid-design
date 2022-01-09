748ff0909d8063712ace062982441b18d80cffc2
pjnalls <pjnalls.js@gmail.com>
2022-01-09 07:16:29 -0600

    `list` → `tasks`.

--- src/app/02-single-responsibility/components/main/main.component.ts
+++ src/app/02-single-responsibility/components/main/main.component.ts
@@ -7,15 +7,15 @@ import { TaskService } from '../../services/task.service';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  list: Array<{ task: string }> = [{ task: '' }];
+  tasks: Array<{ task: string }> = [{ task: '' }];
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.list = this.taskService.list;
+    this.tasks = this.taskService.tasks;
   }
 
   deleteOnClick(index: number): void {
-    this.list.splice(index, 1);
+    this.tasks.splice(index, 1);
   }
 }
