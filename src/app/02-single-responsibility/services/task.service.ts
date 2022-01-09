f49f72c85d0b48d830b46ac7cff0afa429f32335
pjnalls <pjnalls.js@gmail.com>
2022-01-09 07:04:44 -0600

    `list` → `tasks`.

--- src/app/02-single-responsibility/services/task.service.ts
+++ src/app/02-single-responsibility/services/task.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class TaskService {
-  list = [
+  tasks = [
     { task: 'Go to the store.' },
     { task: 'Take out the garbage.' },
     { task: 'Go to sleep early.' },
