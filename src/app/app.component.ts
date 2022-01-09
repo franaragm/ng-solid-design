ecd9eb51ae2ae848e23f4b3f3db80a63e42020bc
pjnalls <pjnalls.js@gmail.com>
2022-01-09 10:20:40 -0600

    Update `title: string` value.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NgSOLID Design';
+  title: string = 'Angular and SOLID Design Principles';
 }
