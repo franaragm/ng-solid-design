a4620cafe0fb97a6dcb537939a2349d2e20ff3ce
pjnalls <pjnalls.js@gmail.com>
2022-01-09 10:24:32 -0600

    + `FooterComponent`.

---
+++ src/app/07-final-app/components/footer/footer.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit {
+  form = this.formBuilder.group({
+    task: '',
+  });
+  submitButtonText = 'ADD TODO';
+  constructor(
+    private taskService: TaskService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.taskService.tasks.length !== 99) {
+      this.taskService.tasks.push(this.form.value);
+      console.log('Task has been added.');
+    }
+  }
+}
